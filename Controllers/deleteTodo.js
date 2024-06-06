009b398c5e19c0da4b5981697e7d204c63212595
dhuruv0003 <116896454+dhuruv0003@users.noreply.github.com>
2024-06-06 19:29:36 +0530

    new commit

--- Controllers/deleteTodo.js
+++ Controllers/deleteTodo.js
@@ -2,8 +2,18 @@ const Todo=require('../Models/Todo')
 
 exports.deleteTodo=async(req,res)=>{
     try {
-        
+        const {id}=req.params;
+        const response=await Todo.findByIdAndDelete(id)
+        res.status(200).json({
+            success:true,
+            data:response,
+            message:('Data Deleted successfully') 
+        })
     } catch (error) {
-        
+        res.status(500).json({
+            success:false,
+            data:"Id Not dfound",
+            message:error.message
+        })
     }
 }
